cf75d27b87071592f8e18dfcbd295b9739663df2
antosusan <handysusanto15@gmail.com>
2022-10-06 22:45:34 +0700

    menambah index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, amet. In quam inventore atque rerum. Quia, saepe corrupti reprehenderit quas beatae voluptate velit, totam eius itaque magnam accusantium cumque fuga.</p>
+</body>
+</html>
